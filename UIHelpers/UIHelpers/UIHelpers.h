55c76e1275459bad08b937ae0ea29c1e19185e66
Alexey Golovenkov <mobidevelop@gmail.com>
2014-10-03 01:36:55 +0400

    Library started

---
+++ UIHelpers/UIHelpers/UIHelpers.h
@@ -0,0 +1,19 @@
+//
+//  UIHelpers.h
+//  UIHelpers
+//
+//  Created by Alexey Golovenkov on 03.10.14.
+//  Copyright (c) 2014 Alexey Golovenkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIHelpers.
+FOUNDATION_EXPORT double UIHelpersVersionNumber;
+
+//! Project version string for UIHelpers.
+FOUNDATION_EXPORT const unsigned char UIHelpersVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIHelpers/PublicHeader.h>
+
+
